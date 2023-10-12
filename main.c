3af3cecde7583cc28507ab89ff505887802ed6ac
gwa8520 <144990431+gwa8520@users.noreply.github.com>
2023-10-12 17:09:27 +0900

    23.10.12 5

--- main.c
+++ main.c
@@ -1,16 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-	
-	void square(int a){
+
+
+	int factorial(int n){
 		
-		a=a*a;
+		int i,res=1;
+		for(i=1;i<=n;i++)
+			res*=i;
+		
+		return res;
 	}
+	
+	
+	int combination(int n, int r){
+		
+		int div1,div2;
+		
+		div1=factorial(n);
+		div2=factorial(n-r)*factorial(r);
+		
+		return(div1/div2);	
+	}
+	
+	
 	
 	int main(int argc, char *argv[]) {
 		
-		int a=2;
-		square(a);					 
-		printf("a=%i\n",a);
-}
-	//result=2, Main and stack are separate blocks and have separate values. Block-to-block interaction can be achieved using 'return'
+		int n,r,result;
+		
+		printf("enter n\n");
+		scanf("%i",&n);
+		
+		printf("enter r\n");
+		scanf("%i",&r);
+		
+		
+		result=combination(n,r);
+		
+		printf("combination is %i",result);
+		
+	return 0;
+	}
+	
