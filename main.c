9f25cc535135dc0ed70eb2c2a7f4120c92bf8a7c
gwa8520 <144990431+gwa8520@users.noreply.github.com>
2023-10-12 16:26:09 +0900

    23.10.12 4

--- main.c
+++ main.c
@@ -2,40 +2,15 @@
 #include <stdlib.h>
 
 	
-	int sumTwo(int a,int b){
-	
-		return a+b;
-	}
-	
-	int squre(int n){
+	void square(int a){
 		
-		return n*n;
-	}
-	
-	int get_max(int x,int y){
-		
-		if(x>y){
-			return x;
-		}
-		else{
-			return y;
-		}
+		a=a*a;
 	}
 	
 	int main(int argc, char *argv[]) {
 		
-		int a,b,n,x,y;
-		a=3;
-		b=10;
-		n=5;
-		x=2;
-		y=7;
-		
-		
-		printf("sumTwo result : %i\n",sumTwo(a,b));
-		printf("squre result : %i\n",squre(n));
-		printf("get max result : %i\n",get_max(x,y));
-		
-	return 0;
+		int a=2;
+		square(a);					 
+		printf("a=%i\n",a);
 }
-	
+	//result=2, Main and stack are separate blocks and have separate values. Block-to-block interaction can be achieved using 'return'
